9b2c247d4a50bf21862569b3463880572bab1269
Ahmet Burak Yazıcı <166993366+ahmetyazc@users.noreply.github.com>
2024-05-16 15:27:34 +0300

    Update machine_learning_example.c

--- machine_learning_example.c
+++ machine_learning_example.c
@@ -30,7 +30,7 @@ int main() {
     float h = 1e-3;
     float ogrenme_katsayisi = 1e-1;
     
-    printf("Baslangic Parametresi: ½f\n", w);
+    printf("Baslangic Parametresi: Â½f\n", w);
     
     for (size_t i = 0; i < 10; ++i) {
     	float turevli_maliyet = (maliyet(w + h) - maliyet(w)) - h;
@@ -38,13 +38,4 @@ int main() {
     	
     	printf("Parametre %f 'a ayarlandi, yeni maliyet: %f\n", w, maliyet(w));
 	}
-    
-	//printf("Parametre maliyeti: %f is %f", w, maliyet(w));
-    // for (size_t i = 0; i < trainingDataSize; ++i) {
-    //     float input = trainingData[i][0];
-    //     float expected_output = trainingData[i][1];
-    //     float actual_output = input * w;
-    //     printf("Expected output: %f, actual output: %f\n", expected_output, actual_output);
-        
-    // }
 }
